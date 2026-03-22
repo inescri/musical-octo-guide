2a27e421a62eb8ec91fab4cf7286b50babc69e15
Crisdell James Ines <crisdelli@codev.com>
2026-03-22 21:50:41 +0800

    step 2

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,10 @@
 import { resolve } from 'node:path'
 import { defineConfig } from 'vite'
 import dts from 'vite-plugin-dts'
-import { nodePolyfills } from 'vite-plugin-node-polyfills'
 
 export default defineConfig({
   plugins: [
     dts(),
-    nodePolyfills({
-      include: ['crypto'],
-    }),
   ],
   build: {
     rollupOptions: {
